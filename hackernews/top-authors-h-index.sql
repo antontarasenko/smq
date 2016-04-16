4f15e394989b231d9e79c33b6c0bc3caa4a77dc1
Anton Tarasenko <antontarasenko@gmail.com>
2016-04-17 00:52:26 +0300

    top hn authors

---
+++ hackernews/top-authors-h-index.sql
@@ -0,0 +1,19 @@
+-- h-index of HN users: accounts for both quality and quantity of submissions
+select
+  author, count(*) h_index
+from (
+  select
+    [by] author, score, rank() over (partition by [by] order by score desc) item_rank
+  from
+    [fh-bigquery:hackernews.full_201510]
+  where
+    type in ('story', 'job', 'poll') and
+    id is not null and
+    score >= 0
+)
+where
+  score >= item_rank
+group by
+  author
+order by
+  h_index desc
